464adf2bf27f3533d4e5295557b1afb2f555065c
Mishra, Utkarsh <utkarsh.mishra01@sap.com>
2024-06-06 15:56:52 +0530

    removing localhost dependency

--- src/Action/baseUrl.js
+++ src/Action/baseUrl.js
@@ -1,4 +1,4 @@
  
- export const baseURL = "http://localhost:3001/api";
+//  export const baseURL = "http://localhost:3001/api";
 // export const baseURL="http://52.55.35.224:3001/api"
-// export const baseURL="https://backend-8rw1.onrender.com/api"
+export const baseURL="https://backend-8rw1.onrender.com/api"
